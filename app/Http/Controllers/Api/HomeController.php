14b1ac9214853afbfcae395d34fac354872dd55f
Durdiev9043 <donyrbek9043@gmail.com>
2025-03-03 19:20:44 +0500

    commit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -20,13 +20,16 @@ class HomeController extends BaseController
         $dd = Daily::where('user_id', $id)->where('day',Carbon::today())->first();
         $time=Carbon::parse('09:00:00');
         $nineAM = Carbon::today()->addHours(9);
-        if (isset($dd) && $dd->created_at->format('H:i:s') > $time){
-            $st=0;
-            $time_interval=0;
-        }else{
-            $st=1;
-            $time_interval=$dd->created_at->diff($nineAM);
+        if ($dd){
+            if ($dd->created_at->format('H:i:s') > $time) {
+                $st = 0;
+                $time_interval = 0;
+            }else {
+                $st = 1;
+                $time_interval = $dd->created_at->diff($nineAM);
+            }
         }
+
         if ($dd) {
             $data = [
                 'come_time' => $dd->created_at->format('H:i:s'),
